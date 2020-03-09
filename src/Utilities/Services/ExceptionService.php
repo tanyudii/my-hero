041aebe64fc2c5954f69f62d8d05fbdbc43a9adb
Yudi Hertanto <y.hertanto17@gmail.com>
2020-03-09 20:39:40 +0700

    init

--- src/Utilities/Services/ExceptionService.php
+++ src/Utilities/Services/ExceptionService.php
@@ -2,16 +2,24 @@
 
 namespace Smoothsystem\Manager\Utilities\Services;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 
 class ExceptionService
 {
-    public static function log(\Exception $e) {
-        Log::error(json_encode([
-            'file' => $e->getFile(),
-            'line' => $e->getLine(),
-            'message' => $e->getMessage(),
+    public static function log($e) {
+        $messages = [
             'timestamp' => now()->toDateTime(),
-        ]));
+        ];
+
+        if (method_exists($e, 'getFile')) $messages['file'] = $e->getFile();
+
+        if (method_exists($e, 'getLine')) $messages['line'] = $e->getLine();
+
+        if (method_exists($e, 'getMessage')) $messages['message'] = $e->getMessage();
+
+        if (Auth::check()) $messages['auth'] = Auth::id();
+
+        Log::error(json_encode($messages));
     }
 }
