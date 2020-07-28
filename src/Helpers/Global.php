5f9e9fe1322758c445c43988d2127e68e130dc6e
Yudi Hertanto <yudi@vodea.id>
2020-07-28 13:54:02 +0700

    add distance

--- src/Helpers/Global.php
+++ src/Helpers/Global.php
@@ -1,1 +1,20 @@
 <?php
+
+if (!function_exists('distance')) {
+    function distance($fromLat, $fromLong, $toLat, $toLong, $unit = 'KM') {
+        $theta = $fromLong - $toLong;
+        $dist = sin(deg2rad($fromLat)) * sin(deg2rad($toLat)) +  cos(deg2rad($fromLat)) * cos(deg2rad($toLat)) * cos(deg2rad($theta));
+        $dist = acos($dist);
+        $dist = rad2deg($dist);
+        $miles = $dist * 60 * 1.1515;
+        $unit = strtoupper($unit);
+
+        switch ($unit) {
+            case 'K':
+            case 'KM':
+                return $miles * 1.609344;
+            default:
+                return $miles;
+        }
+    }
+}
